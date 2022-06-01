f1ec8104254007374074be75ac9949b52b29b19a
Ahmtmrat <yildirimahmetmurat@gmail.com>
2022-06-01 23:43:43 +0300

    Mvc Frontend

---
+++ ProgrammersBlog/ProgrammersBlog/ProgrammersBlog.Mvc/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProgrammersBlog.Services.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.LoadMyServices();
+            services.AddAutoMapper(typeof(Startup));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+           
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                    name:"Admin",
+                    areaName:"Admin",
+                    pattern:"Admin/{controller=Home}/{action=Index}/{id}"
+                );
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ ProgrammersBlog/ProgrammersBlog/ProgrammersBlog.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,49 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        private ResultStatus success;
+        //private ProgrammersBlog.Entities.Dtos.CategoryListDto categoryListDto;
+
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+
+        //public DataResult(ResultStatus success, ProgrammersBlog.Entities.Dtos.CategoryListDto categoryListDto)
+        //{
+        //    this.success = success;
+        //    this.categoryListDto = categoryListDto;
+        //}
+
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+        }
+        public T Data { get; }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+    }
+}
